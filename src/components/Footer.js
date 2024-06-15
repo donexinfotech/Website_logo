9d610404bc5dabbede9ed1b68f024f854575269f
Ankitsm38 <156781349+Ankitsm04@users.noreply.github.com>
2024-06-15 20:26:11 +0530

    added insta link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -40,7 +40,7 @@ export default function Footer() {
   	 			<div class="social-links">
   	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
   	 				<a href="#"><i class="fab fa-twitter"></i></a>
-  	 				<a href="#"><i class="fab fa-instagram"></i></a>
+  	 				<a href="https://www.instagram.com/donexinfotech/"><i class="fab fa-instagram"></i></a>
   	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
   	 			</div>
   	 		</div>
