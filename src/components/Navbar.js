f0805d45ae5872a2809113633c6fd75354868e60
Ankitsm38 <156781349+Ankitsm04@users.noreply.github.com>
2024-06-15 21:14:21 +0530

    navbar-repossive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
-import { Link } from 'react-scroll';
 
 const Navbar = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <a href='/'>LOGO</a>
-      </div>
-      <ul className="navbar-links">
-        <li className="navbar-item">
-          <a href='/'>Home</a>
-        </li>
-        <li className="navbar-item">
-          <a href='/'>About</a>
-        </li>
-        <li className="navbar-item">
-          <a href='/'>Services</a>
-        </li>
-        <li className="navbar-item">
-          <a href='/'>Contact Us</a>
-        </li>
-        <li className="navbar-item">
-          <a href='/'>Demo</a>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="navbar">
+        <div className="navbar-logo">
+          <a href='/'>LOGO</a>
+        </div>
+        <div className="navbar-toggle" onClick={toggleSidebar}>
+          ☰
+        </div>
+        <ul className={`navbar-links ${sidebarOpen ? 'open' : ''}`}>
+          <li className="navbar-item">
+            <a href='/'>Home</a>
+          </li>
+          <li className="navbar-item">
+            <a href='/'>About</a>
+          </li>
+          <li className="navbar-item">
+            <a href='/'>Services</a>
+          </li>
+          <li className="navbar-item">
+            <a href='/'>Contact Us</a>
+          </li>
+          <li className="navbar-item">
+            <a href='/'>Demo</a>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 };
 
