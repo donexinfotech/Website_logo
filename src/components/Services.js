4e82edd66925e23bb786bdaddca817fba0d00669
Ankitsm38 <156781349+Ankitsm04@users.noreply.github.com>
2024-06-18 09:16:49 +0530

    services-change

--- src/components/Services.js
+++ src/components/Services.js
@@ -10,24 +10,24 @@ const Services = () => {
         <div className="card">
         <img src={img} className="card-img-top" alt=""/>
         <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 className="card-title">Portfolio Websites</h5>
+            <p className="card-text">Designed for artists, photographers, designers, and creative professionals to showcase their work.</p>
             <a href="/" className="btn btn-primary sc-btn">Go somewhere</a>
         </div>
         </div>
         <div className="card">
         <img src={img} className="card-img-top" alt="..."/>
         <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 className="card-title">Business Websites</h5>
+            <p className="card-text">Professional sites to showcase services and contact info, enhancing online presence.</p>
             <a href="/" className="btn btn-primary sc-btn">Go somewhere</a>
         </div>
         </div>
         <div className="card">
         <img src={img} className="card-img-top" alt="..."/>
         <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 className="card-title">Appointment Websites</h5>
+            <p className="card-text">Easy-to-use systems for scheduling appointments and reservations.</p>
             <a href="/" className="btn btn-primary sc-btn">Go somewhere</a>
         </div>
         </div>
